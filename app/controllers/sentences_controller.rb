29a6b45abdd312f33112dd51523beaf626d63e87
Tony Lee <tonychunwei@gmail.com>
2013-09-08 01:53:46 -0700

    edit

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -2,6 +2,6 @@ class SentencesController < ApplicationController
   def hello
     @Saying = {:Say => "hello tony!"}
     @Response = {:Response => @Saying}
-    render :xml =>  @Response.to_xml(:root => 'Response')
+    render :xml =>  @Response.to_xml(:root => @Saying)
   end
 end
