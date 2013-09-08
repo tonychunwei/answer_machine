a44d735d30f99d4e0456d5dc2b88ffcea3575e70
Tony Lee <tonychunwei@gmail.com>
2013-09-08 02:29:27 -0700

    add twilio ruby

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -2,5 +2,19 @@ class SentencesController < ApplicationController
   def hello
     @Response = {:Say => "hello tony!"}
     render :xml =>  @Response.to_xml(:root => 'Response')
+    
+    require 'rubygems'
+    require 'twilio-ruby'
+
+    @account_sid = 'AC95988675da3aae714a593e96e258e3eb'
+    @auth_token = '14eac5c5c5329f2bf44c375f2cdeb5b9'
+
+    # set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+
+
+    @account = @client.account
+    @call = @account.calls.create({:from => '+16505427153', :to => '6508620477'})
+    puts @call
   end
 end
