702c3a81895c6aa2a1d7d9a310ac4efef7376ca5
Tony Lee <tonychunwei@gmail.com>
2013-09-08 00:47:31 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnswerMachine::Application.config.secret_token = '7a1d4156cc9bf91e18788568e40aeccb077e9f0256f9f7f2984643e2085b19a9c19115dbeafbd8c8967ecc0ffb39fe3dd1a6012672da0356431a3cdb2688b8a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AnswerMachine::Application.config.session_store :cookie_store, key: '_answer_machine_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AnswerMachine::Application.config.session_store :active_record_store
